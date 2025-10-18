23fa2574530c8d6bcd925f248e15b70a944c1402
PardiniO <pardinioriana@gmail.com>
2025-10-18 01:09:17 -0300

    Creación de rutas: note, highlight

---
+++ backend/src/routes/highlightRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { HighlightController } from "../controllers/highlightController";
+import { ValidationMiddleware } from "../middlewares";
+
+export class HighlightRouter {
+    public router: Router;
+    private highlightController: HighlightController;
+
+    constructor() {
+        this.router = Router();
+        this.highlightController = new HighlightController();
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(): void {
+        this.router.post('/', ValidationMiddleware.validateHighlightCreate, this.highlightController.create);
+        this.router.get('/:id', this.highlightController.getById);
+        this.router.put('/:id', ValidationMiddleware.validateHighlightUpdate, this.highlightController.update);
+        this.router.delete('/:id', this.highlightController.delete);
+        this.router.get('/progress/:progressId', this.highlightController.getByProgress);
+        this.router.get('/search', this.highlightController.search);
+    }
+
+    public geRouter(): Router {
+        return this.router;
+    }
+}
---
+++ backend/src/routes/noteRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { NoteController } from "../controllers/noteController";
+import { ValidationMiddleware } from "../middlewares";
+
+export class NoteRouter {
+    public router: Router;
+    private noteController: NoteController;
+
+    constructor() {
+        this.router = Router();
+        this.noteController = new NoteController();
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(): void {
+        this.router.post('/', ValidationMiddleware.validateNoteCreate, this.noteController.create);
+        this.router.get('/:id', this.noteController.getById);
+        this.router.put('/:id', ValidationMiddleware.validateNoteUpdate, this.noteController.update);
+        this.router.delete('/:id', this.noteController.delete);
+        this.router.get('/progress/:progressId', this.noteController.getByProgress);
+        this.router.get('/search', this.noteController.search);
+    }
+
+    public geRouter(): Router {
+        return this.router;
+    }
+}
