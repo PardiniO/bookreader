6d43d2db293e0754ba8268a204facfd7e7e71456
PardiniO <pardinioriana@gmail.com>
2025-10-18 01:02:05 -0300

    Creación de rutas: language

---
+++ backend/src/routes/languageRoutes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { LanguageController } from "../controllers/languageController";
+import { ValidationMiddleware } from "../middlewares";
+
+export class LanguageRouter {
+    public router: Router;
+    private languageController: LanguageController;
+
+    constructor() {
+        this.router = Router();
+        this.languageController = new LanguageController();
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(): void {
+        this.router.get('/', this.languageController.getAll);
+        this.router.post('/', ValidationMiddleware.validateLanguageCreate, this.languageController.create);
+        this.router.get('/:id', this.languageController.getById);
+        this.router.put('/:id', ValidationMiddleware.validateLanguageUpdate, this.languageController.update);
+        this.router.delete('/:id', this.languageController.delete);
+    }
+
+    public geRouter(): Router {
+        return this.router;
+    }
+}
