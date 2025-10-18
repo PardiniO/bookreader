994393dd8dea32be6f1cff7b0e2edb7031f3a4ab
PardiniO <pardinioriana@gmail.com>
2025-10-18 01:10:00 -0300

    Creación de rutas: library

---
+++ backend/src/routes/libraryRoutes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { LibraryController } from "../controllers/libraryController";
+import { ValidationMiddleware } from "../middlewares";
+
+export class LibraryRouter {
+    public router: Router;
+    private libraryController: LibraryController;
+
+    constructor() {
+        this.router = Router();
+        this.libraryController = new LibraryController();
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(): void {
+        this.router.get('/', this.libraryController.getByUser); // expects auth/userId
+        this.router.post('/', ValidationMiddleware.validateLibraryEntryCreate, this.libraryController.create);
+        this.router.get('/:id', this.libraryController.getById);
+        this.router.put('/:id', ValidationMiddleware.validateLibraryEntryUpdate, this.libraryController.update);
+        this.router.delete('/:id', this.libraryController.delete);
+    }
+
+    public geRouter(): Router {
+        return this.router;
+    }
+}
