a7e361081e7c37e0de3f714d65d6519762c53faf
PardiniO <pardinioriana@gmail.com>
2025-10-18 01:04:29 -0300

    Creación de rutas: nationality

---
+++ backend/src/routes/nationalityRoutes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { NationalityController } from "../controllers/nationalityController";
+import { ValidationMiddleware } from "../middlewares";
+
+export class NationalityRouter {
+    public router: Router;
+    private nationalityController: NationalityController;
+
+    constructor() {
+        this.router = Router();
+        this.nationalityController = new NationalityController();
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(): void {
+        this.router.get('/', this.nationalityController.getAll);
+        this.router.post('/', ValidationMiddleware.validateNationalityCreate, this.nationalityController.create);
+        this.router.get('/:id', this.nationalityController.getById);
+        this.router.put('/:id', ValidationMiddleware.validateNationalityUpdate, this.nationalityController.update);
+        this.router.delete('/:id', this.nationalityController.delete);
+    }
+
+    public geRouter(): Router {
+        return this.router;
+    }
+}
