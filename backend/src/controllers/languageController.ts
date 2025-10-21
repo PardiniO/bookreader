4b58917352a8364f3dc58dcb512022c3f66ac096
PardiniO <pardinioriana@gmail.com>
2025-10-21 00:16:50 -0300

    Creación de controller de idioma

---
+++ backend/src/controllers/languageController.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from 'express';
+import { LanguageModel } from '../models/book/languageModel';
+import { BaseController } from './baseController';
+import { ILanguage } from '../interfaces';
+
+export class LanguageController extends BaseController {
+    private languageModel: LanguageModel;
+
+    constructor() {
+        super();
+        this.languageModel = new LanguageModel();
+    }
+
+    public getAll = async (req: Request, res: Response): Promise<void> => {
+        await this.handleAsyncRoute(req, res, async (req, res) => {
+            const languages = await this.languageModel.getAllLanguages();
+            this.sendSuccess(res, 'Idiomas obtenidos exitosamente', languages);
+        });
+    };
+
+    public getById = async (req: Request, res: Response): Promise<void> => {
+        await this.handleAsyncRoute(req, res, async (req, res) => {
+            const { id } = req.params;
+            if (!this.isValidId(id)) {
+                this.sendError(res, 'ID de idioma inválido');
+                return;
+            }
+            const language = await this.languageModel.getLanguageById(parseInt(id));
+            if (!language) {
+                this.sendNotFound(res, 'Idioma no encontrado');
+                return;
+            }
+            this.sendSuccess(res, 'Idioma obtenido exitosamente', language);
+        });
+    };
+
+    public create = async (req: Request, res: Response): Promise<void> => {
+        await this.handleAsyncRoute(req, res, async (req, res) => {
+            if (!this.validateRequest(req, res)) return;
+            const languageData: Omit<ILanguage, 'id'> = req.body;
+            const languageId = await this.languageModel.createLanguage(languageData);
+            this.sendSuccess(res, 'Idioma creado exitosamente', { id: languageId }, 201);
+        });
+    };
+
+    public update = async (req: Request, res: Response): Promise<void> => {
+        await this.handleAsyncRoute(req, res, async (req, res) => {
+            const { id } = req.params;
+            if (!this.isValidId(id)) {
+                this.sendError(res, 'ID de idioma inválido');
+                return;
+            }
+            if (!this.validateRequest(req, res)) return;
+            const updated = await this.languageModel.updateLanguage(parseInt(id), req.body);
+            if (!updated) {
+                this.sendNotFound(res, 'Idioma no encontrado');
+                return;
+            }
+            this.sendSuccess(res, 'Idioma actualizado exitosamente');
+        });
+    };
+
+    public delete = async (req: Request, res: Response): Promise<void> => {
+        await this.handleAsyncRoute(req, res, async (req, res) => {
+            const { id } = req.params;
+            if (!this.isValidId(id)) {
+                this.sendError(res, 'ID de idioma inválido');
+                return;
+            }
+            const deleted = await this.languageModel.deleteLanguage(parseInt(id));
+            if (!deleted) {
+                this.sendNotFound(res, 'Idioma no encontrado');
+                return;
+            }
+            this.sendSuccess(res, 'Idioma eliminado exitosamente');
+        });
+    };
+}
